6a60757969cf6a742f6d8c51d939ebb088748ce4
Paul Irish <paul.irish@gmail.com>
2010-10-23 11:41:25 -0700

    allow $('elem').css({ 'left' : '+=10px' });

--- cssmonkeypatch.js
+++ cssmonkeypatch.js
@@ -1,4 +1,51 @@
 
+// allow $('elem').css({ 'left' : '+=10px' });
+// so you dont need to do:
+//   $('elem').animate({ 'left' : '+=10px' }, 0);
+// by paul irish
+// for ralph holzmann
+// http://github.com/paulirish/lazyweb-requests/issues#issue/10
+(function($, oldcss) {
+
+    // magic from the core.
+    var rfxnum = /^([+\-]=)?([\d+.\-]+)(.*)$/;
+
+    $.fn.css = function(obj, val) {
+
+
+        var parts = rfxnum.exec(val),
+            that = this;
+
+
+        if ($.isPlainObject(obj)) {
+            $.each(obj, function(k, v) {
+                $(that).css(k, v);
+            });
+
+            // here's the magic.  
+        } else if (val && parts && parts[1]) {
+
+            var end = parseFloat(parts[2])
+
+            return oldcss.call(this, obj, function(index, currentValue) {
+                return ((parts[1] === "-=" ? -1 : 1) * end) + parseFloat(currentValue);
+            });
+
+            // no fancypants  
+        } else {
+
+            return oldcss.apply(this, arguments);
+        }
+
+        return this;
+
+    };
+
+})(jQuery, jQuery.fn.css);
+
+
+
+
 // css() upgrade for a few shorthand values
 // paul irish. MIT license.
 
